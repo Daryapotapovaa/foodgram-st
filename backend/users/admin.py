cefa09f17021724ff2583092687245db1af016c5
Daryapotapovaa <dm.potapova@gmail.com>
2025-05-16 19:45:20 +0300

    admin pannel configured and nginx admin static fix

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser, Subscription
 
-# Register your models here.
+
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    model = CustomUser
+    fieldsets = UserAdmin.fieldsets + (
+        ('Extra', {'fields': ('avatar',)}),
+    )
+
+
+@admin.register(Subscription)
+class SubscriptionAdmin(admin.ModelAdmin):
+    list_display = ('id', 'follower', 'following')
+    search_fields = ('follower__username', 'following__username')
