9a906f25e85da6e7461ce88041a91246a8533ae9
Daryapotapovaa <112759732+Daryapotapovaa@users.noreply.github.com>
2025-05-28 00:59:18 +0300

    loading data fix

--- backend/recipes/management/commands/load_ingredients.py
+++ backend/recipes/management/commands/load_ingredients.py
@@ -8,33 +8,19 @@ class Command(BaseCommand):
     help = 'Загрузка ингредиентов из ingredients.json'
 
     def handle(self, *args, **kwargs):
+        file_path = 'api/preload_data/ingredients.json'
         try:
-            with open('api/preload_data/ingredients.json', encoding='utf-8') as file:
-                ingredients = [
-                    Ingredient(**item) for item in json.load(file)
-                ]
-
-            created = Ingredient.objects.bulk_create(
-                ingredients, ignore_conflicts=True
-            )
+            with open(file_path, encoding='utf-8') as file:
+                created_ingredients = Ingredient.objects.bulk_create(
+                    (Ingredient(**item) for item in json.load(file)),
+                    ignore_conflicts=True
+                )
 
             self.stdout.write(self.style.SUCCESS(
-                f'Загружено {len(created)} новых ингредиентов.'
+                f'Загружено {len(created_ingredients)} новых ингредиентов.'
             ))
 
-        except FileNotFoundError:
-            self.stderr.write(self.style.ERROR(
-                'Файл ingredients.json не найден.'
-            ))
-        except json.JSONDecodeError:
-            self.stderr.write(self.style.ERROR(
-                'Ошибка при чтении JSON-файла.'
-            ))
-        except IntegrityError as e:
-            self.stderr.write(self.style.ERROR(
-                f'Ошибка базы данных: {e}'
-            ))
         except Exception as e:
             self.stderr.write(self.style.ERROR(
-                f'Непредвиденная ошибка: {e}'
+                f'Непредвиденная ошибка: {file_path}: {e}'
             ))
